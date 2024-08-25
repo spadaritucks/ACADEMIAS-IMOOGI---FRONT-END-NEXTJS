f90b61fcae4ecf392c68e2ce249768acf8139286
spadaritucks <thiago.spadari02@gmail.com>
2024-08-25 02:15:16 -0300

    commit

--- src/Components/errors/GlobalModal.tsx
+++ src/Components/errors/GlobalModal.tsx
@@ -7,7 +7,7 @@ const GlobalModal = () => {
     const { modalShow, modalTitle, modalBody, hideModal } = useModal();
 
     return (
-        <Modal show={modalShow} onHide={hideModal}>
+        <Modal show={modalShow} onHide={hideModal} style={{zIndex:3000, display: 'flex', flexDirection:'column', alignItems:'center'}}>
             <Modal.Header>
                 <Modal.Title>{modalTitle}</Modal.Title>
             </Modal.Header>
